d3bbfd62cc408c30aa487cfb93fe67033bc5aa1e
Bala Krishna <ind.iit.bala@gmail.com>
2017-10-22 16:43:11 +0530

    Initial commit

---
+++ app/src/main/java/com/supreme/krishper/miwok/FamilyFragment.java
@@ -0,0 +1,154 @@
+package com.supreme.krishper.miwok;
+
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FamilyFragment extends Fragment {
+
+    private MediaPlayer mMediaplayer;
+    //handles audiofocus when playing a audio file
+    private AudioManager mAudioManager;
+
+    /**
+     +     * This listener gets triggered whenever the audio focus changes
+     +     * (i.e., we gain or lose audio focus because of another app or device).
+     +     */
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                // The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a
+                // short amount of time. The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that
+                // our app is allowed to continue playing sound but at a lower volume. We'll treat
+                // both cases the same way because our app is playing short sound files.
+
+                // Pause playback and reset player to the start of the file. That way, we can
+                // play the word from the beginning when we resume playback.
+                mMediaplayer.pause();
+                mMediaplayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                // The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
+                mMediaplayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                // The AUDIOFOCUS_LOSS case means we've lost audio focus and
+                // Stop playback and clean up resources
+                releaseMediaPlayer();
+            }
+        }
+    };
+    private MediaPlayer.OnCompletionListener mCompletionListener= new  MediaPlayer.OnCompletionListener(){
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
+    public FamilyFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.word_list,container,false);
+        //create and  setup the {@link AudioManger} service to request audio focus
+        mAudioManager = (AudioManager)getActivity().getSystemService(Context.AUDIO_SERVICE);
+        //Initialize the numbers
+        final ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("father","әpә",R.drawable.family_father,R.raw.family_father));
+        words.add(new Word("mother","әṭa",R.drawable.family_mother,R.raw.family_mother));
+        words.add(new Word("son","angsi",R.drawable.family_son,R.raw.family_son));
+        words.add(new Word("daughter","tune",R.drawable.family_daughter,R.raw.family_daughter));
+        words.add(new Word("older brother","taachi",R.drawable.family_older_brother,R.raw.family_older_brother));
+        words.add(new Word("younger brother","chalitti",R.drawable.family_younger_brother,R.raw.family_younger_brother));
+        words.add(new Word("older sister","teṭe",R.drawable.family_older_sister,R.raw.family_older_sister));
+        words.add(new Word("younger sister","kolliti",R.drawable.family_younger_sister,R.raw.family_younger_sister));
+        words.add(new Word("grandmother","ama",R.drawable.family_grandmother,R.raw.family_grandmother));
+        words.add(new Word("grandfather","paapa",R.drawable.family_grandfather,R.drawable.family_grandfather));
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+
+        WordAdapter adapter = new WordAdapter(getActivity(), words,R.color.category_family);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // word_listyout file.
+        ListView listview = (ListView) rootView.findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                Word word = words.get(position);
+
+                releaseMediaPlayer();
+                // Request audio focus for playback
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        // Use the music stream.
+                        AudioManager.STREAM_MUSIC,
+                        // Request temporary focus.
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // Start playback
+                    mMediaplayer = MediaPlayer.create(getActivity(), word.getAudioResourceId());
+                    mMediaplayer.start();
+
+                    mMediaplayer.setOnCompletionListener(mCompletionListener);
+                }
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaplayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaplayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaplayer = null;
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/supreme/krishper/miwok/Word.java
@@ -0,0 +1,45 @@
+package com.supreme.krishper.miwok;
+
+/**
+ * Created by Krish on 7/29/2017.
+ */
+
+public class Word {
+    private  String defaultTranslation;
+    private  String miwokTranslation;
+    private int imageResourceId = NO_IMAGE_PROVIDED;
+    private int audioResourceId;
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId){
+        this.defaultTranslation = defaultTranslation;
+        this.miwokTranslation = miwokTranslation;
+        this.audioResourceId = audioResourceId;
+    }
+
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId){
+        this.defaultTranslation = defaultTranslation;
+        this.miwokTranslation = miwokTranslation;
+        this.imageResourceId = imageResourceId;
+        this.audioResourceId = audioResourceId;
+    }
+
+    public String getDefaultTranslation() {
+        return defaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return miwokTranslation;
+    }
+    public int getImageResourceId(){
+        return imageResourceId;
+    }
+    public boolean hasImage(){
+
+        return imageResourceId!=NO_IMAGE_PROVIDED;
+    }
+    public int getAudioResourceId(){
+        return audioResourceId;
+    }
+}
